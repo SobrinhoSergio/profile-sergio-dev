4f05655b9ae2ef878f78f65489f33d9f0d9fc82d
Sérgio Sobrinho <sobrinhosergio00@gamil.com>
2022-04-22 14:49:32 -0300

    Ordem pastas

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,5 +1,5 @@
-export const bg = "../bg.jpg";
-export const bg1 = "../bg1.jpg";
-export const bg2 = "../bg2.jpg";
-export const profile = "../profile.jpg";
-export const curriculo = "../curriculo.pdf";
+export const bg = "../assets/imagens/bg.jpg";
+export const bg1 = "../assets/imagens/bg1.jpg";
+export const bg2 = "../assets/imagens/bg2.jpg";
+export const profile = "../assets/imagens/profile.jpg";
+export const curriculo = "../assets/curriculo.pdf";
